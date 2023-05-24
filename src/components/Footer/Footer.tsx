448d7e4cca253ed33e9b888d55c1b86e9d5e7edb
Ilyar <kasyymov.ilyar@gmail.com>
2023-05-24 14:56:10 +0300

    added footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,20 @@
-import React, { FC } from "react";
+import { FC } from "react";
+import { Container } from "react-bootstrap";
+
+import { Logo } from "../";
 
 import "./Footer.scss";
 
 interface FooterProps {}
 
 export const Footer: FC<FooterProps> = () => {
-	return <div>Footer</div>;
+	const currentYear = new Date().getFullYear();
+
+	return (
+		<footer className="p-4 bg-dark text-white header">
+			<Container className="d-flex justify-content-center">
+				<div>© {currentYear}</div>
+			</Container>
+		</footer>
+	);
 };
