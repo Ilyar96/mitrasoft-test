7642ac228e09048af122d8c779d9f12fd859d83d
Ilyar <kasyymov.ilyar@gmail.com>
2023-05-26 13:29:29 +0300

    added lazy import

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,37 @@
-import React from "react";
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { MainLayout } from "../../layouts";
-import { About, Home, User } from "../../pages";
 import { AppRoutes } from "../../types/common";
+
+const Home = lazy(() =>
+	import("../../pages").then((module) => ({
+		default: module.Home,
+	}))
+);
+const About = lazy(() =>
+	import("../../pages").then((module) => ({
+		default: module.About,
+	}))
+);
+const User = lazy(() =>
+	import("../../pages").then((module) => ({
+		default: module.User,
+	}))
+);
 
 export const App = () => {
 	return (
-		<Routes>
-			<Route path={AppRoutes.HOME} element={<MainLayout />}>
-				<Route index element={<Home />} />
-				<Route path={AppRoutes.ABOUT} element={<About />} />
-				<Route path={`${AppRoutes.USERS}/:id`} element={<User />} />
+		<Suspense>
+			<Routes>
+				<Route path={AppRoutes.HOME} element={<MainLayout />}>
+					<Route index element={<Home />} />
+					<Route path={AppRoutes.ABOUT} element={<About />} />
+					<Route path={`${AppRoutes.USERS}/:id`} element={<User />} />
 
-				<Route path="*" element={<Home />} />
-			</Route>
-		</Routes>
+					<Route path="*" element={<Home />} />
+				</Route>
+			</Routes>
+		</Suspense>
 	);
 };
